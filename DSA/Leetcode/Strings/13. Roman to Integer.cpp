7a73bfa4da929ae13fada673e404f4019d77988e
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2022-10-21 16:57:34 +0530

    Create 13. Roman to Integer.cpp

---
+++ DSA/Leetcode/Strings/13. Roman to Integer.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.length();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n && s[i] == 'I' && s[i + 1] == 'V') {
+                sum += 4;
+                i++;
+            }
+            else if (i + 1 < n && s[i] == 'I' && s[i + 1] == 'X') {
+                sum += 9;
+                i++;
+            }
+            else if (i + 1 < n && s[i] == 'X' && s[i + 1] == 'L') {
+                sum += 40;
+                i++;
+            }
+            else if (i + 1 < n && s[i] == 'X' && s[i + 1] == 'C') {
+                sum += 90;
+                i++;
+            }
+            else if (i + 1 < n && s[i] == 'C' && s[i + 1] == 'D') {
+                sum += 400;
+                i++;
+            }
+            else if (i + 1 < n && s[i] == 'C' && s[i + 1] == 'M') {
+                sum += 900;
+                i++;
+            }
+            else if (s[i] == 'I') {
+                sum += 1;
+            }
+            else if (s[i] == 'V') {
+                sum += 5;
+            }
+            else if (s[i] == 'X') {
+                sum += 10;
+            }
+            else if (s[i] == 'L') {
+                sum += 50;
+            }
+            else if (s[i] == 'C') {
+                sum += 100;
+            }
+            else if (s[i] == 'D') {
+                sum += 500;
+            }
+            else if (s[i] == 'M') {
+                sum += 1000;
+            }
+        }
+        return sum;
+    }
+};
