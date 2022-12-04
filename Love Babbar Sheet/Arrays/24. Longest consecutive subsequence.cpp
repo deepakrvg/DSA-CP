74538b8c5abc9b4b100ec441f5019884d396e3c8
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2022-12-04 12:01:20 +0530

    Create 24. Longest consecutive subsequence.cpp

---
+++ Love Babbar Sheet/Arrays/24. Longest consecutive subsequence.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+
+#define int long long
+
+int mod = 1e9 + 7;
+
+
+int findLongestConseqSubseq(int arr[], int N) {
+	sort(arr, arr + N);
+	int ans = 1;
+	int sum = 1;
+	for (int i = 0; i < N; i++) {
+		if (i) {
+			if (arr[i] - arr[i - 1] == 1) {
+				sum++;
+				ans = max(ans, sum);
+			}
+			else if (arr[i] - arr[i - 1] == 0) {
+				continue;
+			}
+			else {
+				sum = 1;
+			}
+		}
+	}
+	return ans;
+}
+
+int abc() {
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	auto ans = findLongestConseqSubseq(arr, n);
+
+	cout << ans;
+
+	cout << "\n";
+	return 0;
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc = 1;
+	// cin >> tc;
+	for (int tt = 0; tt < tc; tt++) 
+	{
+		// cout << "Case #" << (tt + 1) << ": ";
+		abc();
+	}
+ 
+	return 0;
+}
