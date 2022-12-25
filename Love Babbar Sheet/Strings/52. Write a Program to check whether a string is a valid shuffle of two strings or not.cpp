689341be4a721827145a99777e0d5db7ce8dd07d
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2022-12-25 17:04:52 +0530

    Update 52. Write a Program to check whether a string is a valid shuffle of two strings or not.cpp

--- Love Babbar Sheet/Strings/52. Write a Program to check whether a string is a valid shuffle of two strings or not.cpp
+++ Love Babbar Sheet/Strings/52. Write a Program to check whether a string is a valid shuffle of two strings or not.cpp
@@ -1,3 +1,78 @@
+// check if a string is valid shuffle of two strings maintaining the relative ordering
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+
+#define int long long
+#define MAX 256
+
+int mod = 1e9 + 7;
+
+
+
+
+bool solve(string a, string b, string c) {
+	int i = 0, j = 0, k = 0;
+	bool flag = 0;
+	while (k < c.size()) {
+		if (i < a.size() && a[i] == c[k]) {
+			i++;
+		}
+		else if (j < b.size() && b[j] == c[k]) {
+			j++;
+		}
+		else {
+			flag = 1;
+			break;
+		}
+		k++;
+	}
+	if (flag) {
+		return 0;
+	}
+	return 1;
+}
+
+int abc() {
+	string a, b, c;
+	cin >> a >> b >> c;
+
+	auto ans = solve(a, b, c);
+	cout << ans;
+
+	cout << "\n";
+	return 0;
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc = 1;
+	// cin >> tc;
+	for (int tt = 0; tt < tc; tt++) 
+	{
+		// cout << "Case #" << (tt + 1) << ": ";
+		abc();
+	}
+ 
+	return 0;
+}
+
+
+
+/*
+find if string a is shuffled substring of string b
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -84,3 +159,4 @@ int32_t main() {
  
 	return 0;
 }
+*/
