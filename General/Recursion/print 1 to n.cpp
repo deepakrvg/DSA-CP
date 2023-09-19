6f793fbfb40e31231baa74fcb2467cc93de433db
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2023-09-20 00:36:27 +0530

    Create print 1 to n.cpp

---
+++ General/Recursion/print 1 to n.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+
+
+
+
+void rec(int n) {
+    if (n == 1) {
+        cout << 1 << " ";
+        return;
+    }
+    rec(n - 1);
+    cout << n << " ";
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    rec(n);
+}
+
+int32_t main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) { 
+      solve();
+    }
+    return 0;
+}
