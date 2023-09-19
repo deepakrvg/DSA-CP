3198fc6f66ce3b3f2bce5882da2b6dd18d6f581b
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2023-09-20 00:41:53 +0530

    Create factorial.cpp

---
+++ General/Recursion/factorial.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+
+
+
+
+int rec(int n) {
+    // base case
+    if (n == 1) {
+        return 1;
+    }
+
+    // hypothesis
+    int a = rec(n - 1);
+
+    // induction
+    return a * n;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << rec(n);
+}
+
+int32_t main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) { 
+      solve();
+    }
+    return 0;
+}
