c39e074e27767c0b2fbcd8403b33cb21e5b467a0
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2023-10-01 00:48:17 +0530

    Update equal sum partition.cpp

--- General/Dynamic Programming/equal sum partition.cpp
+++ General/Dynamic Programming/equal sum partition.cpp
@@ -1,64 +1,38 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-#define int long long
-
-
-bool subset_sum(int arr[], int sum, int n) {
-    bool dp[n + 1][sum + 1];
-    for (int i = 0; i < n + 1; i++) {
-        for (int j = 0; j < sum + 1; j++) {
-            if (i == 0)
-                dp[i][j] = false;
-            if (j == 0) 
-                dp[i][j] = true;
-        }
-    }
-
-    for (int i = 1; i < n + 1; i++) {
-        for (int j = 1; j < sum + 1; j++) {
-            if (arr[i - 1] <= j) {
-                dp[i][j] = dp[i - 1][j - arr[i - 1]] or dp[i - 1][j];
+class Solution {
+public:
+    bool subset_sum(vector<int> &nums, int sum) {
+        int n = nums.size();
+        int dp[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                if (i == 0)
+                    dp[i][j] = false;
+                if (j == 0)
+                    dp[i][j] = true;
             }
-            else
-                dp[i][j] = dp[i - 1][j];
         }
-    }
-    return dp[n][sum];
-}
 
-void solve() {
-    int n;
-    cin >> n;
-    int arr[n];
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        sum += arr[i];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (nums[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] or dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][sum];
     }
 
-    if (sum % 2 != 0) {
-        cout << 0;
-    }
-    else {
-        cout << subset_sum(arr, sum / 2, n);
-    }
-}
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
 
-int32_t main(){
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int tc = 1;
-    // cin >> tc;
-    for (int t = 1; t <= tc; t++) { 
-      solve();
+        if (sum % 2 != 0)
+            return false;
+        else
+            return subset_sum(nums, sum / 2);
     }
-    return 0;
-}
+};
