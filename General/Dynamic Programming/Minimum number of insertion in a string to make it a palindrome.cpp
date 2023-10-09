1cde9c29da29e18e542374d095d9c5ae6dafd988
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2023-10-10 02:49:41 +0530

    Create Minimum number of insertion in a string to make it a palindrome.cpp

---
+++ General/Dynamic Programming/Minimum number of insertion in a string to make it a palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        string a = s;
+        string b = s;
+        reverse(b.begin(), b.end());
+        int n = a.length();
+        int t[n + 1][n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            t[i][0] = 0;
+        }
+        for (int j = 0; j < n + 1; j++) {
+            t[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (a[i - 1] == b[j - 1]) {
+                    t[i][j] = t[i - 1][j - 1] + 1;
+                }
+                else {
+                    t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+                }
+            }
+        }
+        return n - t[n][n];
+    }
+};
