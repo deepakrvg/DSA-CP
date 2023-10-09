9f45eb1df73c38a39adc01812b0ddb1346c34c14
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2023-10-10 04:01:28 +0530

    Update Minimum number of deletions and insertions.cpp

--- General/Dynamic Programming/Minimum number of deletions and insertions.cpp
+++ General/Dynamic Programming/Minimum number of deletions and insertions.cpp
@@ -1,52 +1,37 @@
-// Program to find minimum number of platforms
-// required on a railway station
-#include <algorithm>
-#include <iostream>
+class Solution{
+		
 
-using namespace std;
-
-// Returns minimum number of platforms required
-int findPlatform(int arr[], int dep[], int n)
-{
-	// Sort arrival and departure arrays
-	sort(arr, arr + n);
-	sort(dep, dep + n);
-
-	// plat_needed indicates number of platforms
-	// needed at a time
-	int plat_needed = 1, result = 1;
-	int i = 1, j = 0;
-
-	// Similar to merge in merge sort to process
-	// all events in sorted order
-	while (i < n && j < n) {
-		// If next event in sorted order is arrival,
-		// increment count of platforms needed
-		if (arr[i] <= dep[j]) {
-			plat_needed++;
-			i++;
-		}
-
-		// Else decrement count of platforms needed
-		else if (arr[i] > dep[j]) {
-			plat_needed--;
-			j++;
-		}
-
-		// Update result if needed
-		if (plat_needed > result)
-			result = plat_needed;
-	}
-
-	return result;
-}
-
-// Driver code
-int main()
-{
-	int arr[] = { 900, 940, 950, 1100, 1500, 1800 };
-	int dep[] = { 910, 1200, 1120, 1130, 1900, 2000 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << findPlatform(arr, dep, n);
-	return 0;
-}
+	public:
+	int minOperations(string str1, string str2) 
+	{ 
+	    // Your code goes here
+	    int n = str1.length();
+	    int m = str2.length();
+	    int t[n + 1][m + 1];
+	    
+	    for (int i = 0; i < n + 1; i++) {
+	        t[i][0] = 0;
+	    }
+	    for (int j = 0; j < m + 1; j++) {
+	        t[0][j] = 0;
+	    }
+	    
+	    for (int i = 1; i < n + 1; i++) {
+	        for (int j = 1; j < m + 1; j++) {
+	            if (str1[i - 1] == str2[j - 1]) {
+	                t[i][j] = t[i - 1][j - 1] + 1;
+	            }
+	            else {
+	                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+	            }
+	        }
+	    }
+	    int lcs = t[n][m];
+	    
+	    // number of insertions
+	    int insertions = n - lcs;
+	    // number of deletions
+	    int deletions = m - lcs;
+	    return insertions + deletions;
+	} 
+};
