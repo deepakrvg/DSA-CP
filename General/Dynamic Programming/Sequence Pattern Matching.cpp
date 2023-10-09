eaf49dc8368a3fa78af15ad50d912e0b992a4ae6
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2023-10-10 02:02:36 +0530

    Create Sequence Pattern Matching.cpp

---
+++ General/Dynamic Programming/Sequence Pattern Matching.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        int dp[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < m + 1; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m] == n;
+    }
+};
