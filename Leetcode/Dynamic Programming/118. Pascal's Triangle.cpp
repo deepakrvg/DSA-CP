53382f6ae05d671760eff36d214c997e1e691965
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2023-12-03 19:52:17 +0530

    Create 118. Pascal's Triangle.cpp

---
+++ Leetcode/Dynamic Programming/118. Pascal's Triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans(numRows);
+        for (int i = 0; i < numRows; i++) {
+            ans[i].resize(i + 1);
+            for (int j = 0; j <= i; j++) {
+                if (i == j or j == 0) {
+                    ans[i][j] = 1;
+                }
+                else {
+                    ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
+                }
+            }
+        }
+        return ans;
+    }
+};
